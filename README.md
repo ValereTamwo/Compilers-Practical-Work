90bf3cd378cc4c3ab3e76647c8a64274b47b7943
folong201 <folong201@gmail.com>
2024-06-28 05:39:52 +0100

    update code structure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Compilers-Practical-Work
+
+## Contributors
+- [Folong Emerson](mailto:folong201@gmail.com) (Matricule: 20U2998)
--- tp1/exo1/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int count_occurrences(FILE *file, const char *target);
-FILE *open_file(const char *filename);
-void close_file(FILE *file);
-
-#endif
